7911e1a22c1ac93f60cb989f9ee262c3561938a7
Lubos Jones <l.furinda@gmail.com>
2020-01-17 06:46:31 +0100

    Update menu items with react router

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import React from 'react';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
+
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Router>
+        <Switch>
+       <Route exact path='/'>
+          <HomePage />
+       </Route>
+       </Switch>
+      </Router>
     </div>
   );
 }
