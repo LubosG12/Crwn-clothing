859aae65c04a147ff9f76f7585c2022813a8feb3
Lubos Jones <l.furinda@gmail.com>
2020-01-17 07:44:13 +0100

    shop page added without styling and components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
-import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component';
+
 
 
 function App() {
   return (
     <div>
-      <Router>
         <Switch>
-       <Route exact path='/'>
-          <HomePage />
-       </Route>
+         <Route exact path='/'>
+            <HomePage />
+         </Route>
+         <Route exact path='/shop'>
+            <ShopPage />
+         </Route>
        </Switch>
-      </Router>
     </div>
   );
 }
