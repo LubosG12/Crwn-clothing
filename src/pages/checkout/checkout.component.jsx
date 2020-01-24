d9604d312a3c8db7c81d1f3987cbdfd216118fe3
Lubos Jones <l.furinda@gmail.com>
2020-01-24 12:44:52 +0100

    Added remove item from cart action and functionality to arrows in checkout page

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -27,8 +27,11 @@ const CheckoutPage = ({ cartItems, cartTotal}) => (
                 <span>Remove</span>
             </div>
         </div>
-        {
-            cartItems.map(cartItem=> (<CheckoutItem key={cartItem.id} cartItem={cartItem}/>))
+        { 
+        cartItems.length>0 ?  
+            cartItems.map(cartItem=> (<CheckoutItem key={cartItem.id} cartItem={cartItem}/>)) 
+            :
+            <span className="empty-message">Your cart is empty</span>
         }
         <div className='total'>
             <span>TOTAL: ${cartTotal}</span>
