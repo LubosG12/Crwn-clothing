1ece82e34437c5f23e1e7d63b05d12e88de582a0
Lubos Jones <l.furinda@gmail.com>
2020-01-24 17:42:26 +0100

    Redux persist added to keep track of session storage

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -10,6 +10,7 @@ const Footer = () => (
             <span>Fake Street 123</span>
             <span>Fake Town</span>
             <span>+421901234567</span>
+            <span>customersupport@crwn.com</span>
         </div>
         <div className="right-block">
             <span>Facebook</span>
