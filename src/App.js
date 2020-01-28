c20f150b74b2cad073d244b4ec3a1b8771f7ab65
Lubos Jones <l.furinda@gmail.com>
2020-01-28 15:56:55 +0100

    Added full firebase intergration with the item database

--- src/App.js
+++ src/App.js
@@ -40,8 +40,8 @@ class App extends React.Component {
          });
        } else {
        setCurrentUser(userAuth);
-      // addCollectionAndDocuments('collections', collectionsArray.map(({ title, items }) => ({ title, items })))
-     }
+       // addCollectionAndDocuments('collections', collectionsArray.map(({ title, items }) => ({ title, items })))
+    }
     })
   }
 
--- src/redux/shop/shop.reducer.js
+++ src/redux/shop/shop.reducer.js
@@ -1,11 +1,18 @@
 import SHOP_DATA from './shop.data';
 
+import ShopActionTypes from './shop.types';
+
 const INIT_STATE = {
     collections: SHOP_DATA
 }
 
 const shopReducer = (state = INIT_STATE, action) => {
     switch(action.type) {
+        case ShopActionTypes.UPDATE_COLLECTIONS: 
+        return {
+            ...state,
+            collections: action.payload
+        }
         default: 
         return state;
     }
